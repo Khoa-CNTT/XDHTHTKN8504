6bf5ade79b0631eccc7f72992208f3fbf6a80d68
LuuNv025 <luu.nguyenvan2511@gmail.com>
2025-04-15 19:02:03 +0700

    fix login ui

--- elder-care-staff-mobile/app/index.tsx
+++ elder-care-staff-mobile/app/index.tsx
@@ -1,10 +1,22 @@
-import { View } from "react-native";
-import { Link } from "expo-router";
+// app/index.tsx
+import { useEffect } from "react";
+import { Redirect } from "expo-router";
+import useAuthStore from "./stores/authStore";
 
 export default function Index() {
-  return (
-    <View>
-      <Link href="/screens/tabs/home">Click Here</Link>
-    </View>
-  );
+  const token = useAuthStore((state) => state.token);
+  const restoreSession = useAuthStore((state) => state.restoreSession);
+  const loading = useAuthStore((state) => state.loading);
+
+  useEffect(() => {
+    restoreSession();
+  }, []);
+
+  if (loading) return null; // hoặc bạn có thể show loading UI
+  // Nếu đã đăng nhập, chuyển hướng tới màn chính
+  if (token) {
+    return <Redirect href="/screens/tabs/home" />;
+  }
+  // Nếu chưa, chuyển hướng tới trang login
+  return <Redirect href="/screens/auth/Login" />;
 }
