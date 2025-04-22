13ab6795c023ada35054af42f47344ec533313f0
Dinh Long <dinhlong366@gmail.com>
2025-04-22 19:27:09 +0700

    Chức năng Upload ảnh

--- elder-care-api/src/routes/authRouter.js
+++ elder-care-api/src/routes/authRouter.js
@@ -1,8 +1,20 @@
 import express from "express";
 import authController from "../controllers/authController.js";
+import upload from '../middlewares/upload.js';
+import auth from "../middlewares/auth.js";
+import authorizeRoles from "../middlewares/authorizeRoles.js";
 
 const router = express.Router();
 
 router.post("/signup", authController.registerUser);
 router.post("/login", authController.loginUser);
+
+router.post(
+    "/upload", 
+    auth,
+    upload.single("avatar"),
+    authorizeRoles("admin", "doctor", "nurse", "family-member"), 
+    authController.uploadAvatar
+);
+
 export default router;
