3e8df16724ea32ec9282f17e7c7f157992d0c125
Dinh Long <dinhlong366@gmail.com>
2025-04-27 20:32:03 +0700

    Api get Profile cho app customer

--- elder-care-api/src/routes/profileRouter.js
+++ elder-care-api/src/routes/profileRouter.js
@@ -1,8 +1,17 @@
 import profileController from '../controllers/profileController.js';
 import express from 'express';
+import auth from '../middlewares/auth.js';
+import authorizeRoles from '../middlewares/authorizeRoles.js';
 
 const router = express.Router();
 
 router.post('/create', profileController.createProfile);
+
+router.get(
+    '/get-profiles',
+    auth,
+    authorizeRoles("admin", "family_member"),
+    profileController.getUserProfiles
+)
 
 export default router;
