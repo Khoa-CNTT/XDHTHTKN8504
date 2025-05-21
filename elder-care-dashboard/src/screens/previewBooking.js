39bd99a3b5b0baf208f1e4ed618c39f2ccdb6698
Dinh Long <dinhlong366@gmail.com>
2025-05-21 20:43:03 +0700

    api cho app

--- elder-care-dashboard/src/screens/previewBooking.js
+++ elder-care-dashboard/src/screens/previewBooking.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "../Layout"
 
 function PreviewBooking() {
-  return <div>trongdrfhruifrf rhfrufbrfyrf</div>;
+  return (
+    <Layout>
+      <h1 className="text-xl font-semibold">Booking Detail</h1>
+    </Layout>
+  )
 }
 
 export default PreviewBooking;
